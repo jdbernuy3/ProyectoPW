480f2ecf945ea28877a24670dc57a54c500d914b
grandaz <20200945@aloe.ulima.edu.pe>
2023-10-10 18:17:42 -0500

    tabss

--- src/app/components/MyTabPanel/MyTabPanel.jsx
+++ src/app/components/MyTabPanel/MyTabPanel.jsx
@@ -6,63 +6,74 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
 CustomTabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
 }
 
-export default function MyTabPanel() {
-  const [value, setValue] = React.useState(0);
+export default function MyTabPanel(props) {
+    const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 0, borderColor:'divider'}}>
-        <Tabs value={value} onChange={handleChange}  textColor="secondary" indicatorColor="secondary" aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} sx={{color:'#6750A4'}}/>
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
-        Item One
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Item Three
-      </CustomTabPanel>
-    </Box>
-  );
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 0, borderColor: 'divider', borderRadius: 5, backgroundColor: '#FEF7FF' }}>
+                <Tabs value={value} onChange={handleChange} textColor="secondary" indicatorColor="secondary" aria-label="basic tabs example">
+                    {
+                        props.tabs.map((element) =>
+                            <Tab label={element.text} {...a11yProps(element.id)} />
+                        )
+                    }
+                </Tabs>
+            </Box>
+
+            {
+                props.tabs.map((element) =>
+                    <CustomTabPanel value={value} index={element.id}>
+                        {element.contenido}
+                    </CustomTabPanel>
+                )
+            }
+
+            <CustomTabPanel value={value} index={0}>
+                Item One
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+                Item Two
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={2}>
+                Item Three
+            </CustomTabPanel>
+        </Box>
+    );
 }
--- src/app/eliminarRecurso/page.jsx
+++ src/app/eliminarRecurso/page.jsx
@@ -2,7 +2,35 @@
 import MyAppBar from "../components/MyAppBar/MyAppBar"
 import MyTabPanel from "../components/MyTabPanel/MyTabPanel"
 
+function Contenido1() {
+    return (
+        <>
+        <h1>HOLAAAAAAAAAAAAAA</h1>
+        </>
+    )
+}
+
+function Contenido2() {
+    return (
+        <>
+        <h1>asdasdasdasd</h1>
+        </>
+    )
+}
+
 function EliminarRecurso() {
+
+    const tabs = [
+        {
+            id: 0,
+            text: 'Hola',
+        },
+        {
+            id: 1,
+            text: 'Hola2',
+        }
+    ]
+
     return (
         <>
         <MyAppBar></MyAppBar>
@@ -10,7 +38,7 @@ function EliminarRecurso() {
             <h1>Hola, Juliana</h1>
             <hr className="my-8 h-0.5 border-t-0 bg-[#CAC4D0] opacity-100" />
             <div className="h-100v w-100v bg-[#F3EDF7]">
-            <MyTabPanel></MyTabPanel>
+            <MyTabPanel tabs={tabs} contenido></MyTabPanel>
             </div>
         </div>
         </>
