dcc8807c341d7ba94b294a2478eb1b86a19db1c9
Juan Diego Bernuy Rey <JD>
2023-10-09 22:52:20 -0500

    jd1

---
+++ src/app/Estudiantes/Estudiante.jsx
@@ -0,0 +1,24 @@
+
+
+
+return (
+    <>
+        <h1>Mi Perfil</h1>
+        {
+            /* Agregar Barra de Navegacion; Datos Personales y Cuenta */ 
+        }
+
+
+
+
+
+        <input>
+        </input>
+        <br />
+        <button>Guardar</button>
+        
+    </>
+)
+}
+
+export default Alumnos
---
+++ src/pages/MiPerfil.js
@@ -0,0 +1,9 @@
+import Estudiante from '/../app/Estudiantes/Estudiante.jsx'
+
+const MiPerfilEstudiante = () => {
+    return (
+        <Estudiante />
+    )
+}
+
+export default MiPerfilEstudiante
