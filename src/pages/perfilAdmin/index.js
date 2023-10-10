884d2d75aa07e7aa3864bba8ffec71595c1634cb
grandaz <20200945@aloe.ulima.edu.pe>
2023-10-10 08:36:12 -0500

    a

---
+++ src/pages/perfilAdmin/index.js
@@ -0,0 +1,17 @@
+import NavBarAdministradorPerfil from "../../Componentes/BarrasDeNavegacion/NavBarAdministradorPerfil"
+
+function AdminPerfil()
+{
+    return 
+    (
+        <>
+        <h1>Pagina Admin</h1>
+        <NavBarAdministradorPerfil> </NavBarAdministradorPerfil>
+        </>
+        
+    );
+
+
+}
+
+export default AdminPerfil
---
+++ src/pages/perfilEstudiante/index.js
@@ -0,0 +1,15 @@
+import NavBarEstudiantePerfil from "../../Componentes/BarrasDeNavegacion/NavBarEstudiantePerfil"
+
+function EstudiantePerfil()
+{
+    return (
+        <>
+        <h1>Pagina Estudiante</h1>
+    <NavBarEstudiantePerfil></NavBarEstudiantePerfil>
+        </>
+    
+    )
+
+}
+
+export default EstudiantePerfil
