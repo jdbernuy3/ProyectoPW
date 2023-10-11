a4b7155a73fe6ea74e8d6f230d8934042d676bd5
MariaPaz <20200579@aloe.ulima.edu.pe>
2023-10-10 23:45:51 -0500

    interfaz buscador

--- src/app/busqueda/page.jsx
+++ src/app/busqueda/page.jsx
@@ -1,27 +1,37 @@
-'use client'
-import MyAppBar from "../components/MyAppBar/MyAppBar"
-import MyTabPanel from "../components/MyTabPanel/MyTabPanel"
+'use client';
+import FormDatosPersonales from "@/app/components/FormDatosPersonales/FormDatosPersonales";
+import MyAppBar from "@/app/components/MyAppBar/MyAppBar";
+import MyTabPanel from "@/app/components/MyTabPanel/MyTabPanel";
+import FormCuenta from "@/app/components/FormCuenta/FormCuenta";
+import FormPreferencias from "@/app/components/FormPreferencias/FormPreferencias";
 import FormInsertarRecurso from "../components/FormInsertarRecurso/FormInsertarRecurso"
+import InsertarRecurso from "../components/FormInsertarRecursoBusqueda/FormInsertarRecursoBusqueda";
+import FormInsertarRecursoBusqueda from "../components/FormInsertarRecursoBusqueda/FormInsertarRecursoBusqueda";
 
 
-function busquedaAdm() {
+    const busquedaAdm = () => {
 
+        const tabs = [
+            {
+                id: 0,
+                contenido:<FormInsertarRecursoBusqueda></FormInsertarRecursoBusqueda>
+            },
+        ]
+    
+        return (
+            <>
+                <MyAppBar></MyAppBar>
+                <div className="bg-white h-100v w-100v pl-52 pr-8">
+                    <h1 className="pt-10 text-4xl">Busqueda</h1>
+                    <hr className="my-8 h-0.5 border-t-0 bg-[#CAC4D0] opacity-100" />
+                    <div className="h-100v w-100v bg-[#F3EDF7] flex p-10 space-x-2">
 
-    return (
-        <>
-            <MyAppBar></MyAppBar>
-            <div className="bg-white h-100v w-100v pl-52 pr-8">
-                <h1 className="pt-10 text-4xl">Busqueda</h1>
-                <hr className="my-8 h-0.5 border-t-0 bg-[#CAC4D0] opacity-100" />
-
-                <div className="h-100v w-100v bg-[#F3EDF7] flex p-10 space-x-2">
-                    <div className="">
                         <MyTabPanel tabs={tabs}></MyTabPanel>
                     </div>
                 </div>
-            </div>
-        </>
-    )
-}
+            </>
+        )
+    } 
+    
 
 export default busquedaAdm
---
+++ src/app/components/FormInsertarRecursoBusqueda/FormInsertarRecursoBusqueda.jsx
@@ -0,0 +1,79 @@
+
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import FilledInput from '@mui/material/FilledInput';
+import FormHelperText from '@mui/material/FormHelperText';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+function FormInsertarRecursoBusqueda() {
+    return (
+        <>
+        <div className='text-left'>
+            <ul>
+        <li className='p-4'>
+        <TextField
+          label="Ingresa la palabra clave"
+          id="outlined-start-adornment"
+          InputProps={{
+            startAdornment: <InputAdornment position="start" ><SearchIcon /></InputAdornment>,
+          }}
+          variant="outlined"
+        />
+                    </li>
+
+                <li className='p-4'>
+                <TextField
+          label="Tipo de recurso"
+          id="outlined-start-adornment"
+          InputProps={{
+            startAdornment: <InputAdornment position="start" ><SearchIcon /></InputAdornment>,
+          }}
+          variant="outlined"
+        />
+                </li>
+             
+            </ul>
+           
+        </div>
+        <div className='text-right'>
+        <button class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full focus:ring-0" >Limpiar</button>
+            <button class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full focus:ring-0">Buscar</button>
+
+        </div>
+        </>
+    )
+}
+
+export default FormInsertarRecursoBusqueda
+
+
+
+{/*
+<FormControl fullWidth  variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            startAdornment={<InputAdornment position="start">icono</InputAdornment>}
+            labelWidth={60}
+          />
+        </FormControl>
+
+        
+
+<FormControl fullWidth  variant="outlined" >
+             <FilledInput
+            id="filled-adornment-weight"
+            startAdornment={<InputAdornment position="start">icono</InputAdornment>}
+            aria-describedby="filled-weight-helper-text"
+            inputProps={{
+              'aria-label': 'weight',
+            }}
+            />
+            <FormHelperText id="filled-weight-helper-text">Ingrese la palabra clave</FormHelperText>
+          </FormControl>
+
+*/}
