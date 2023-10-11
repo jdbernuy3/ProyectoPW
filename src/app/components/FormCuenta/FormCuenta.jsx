62f6cb434c19b4e4d415b6c01c82b049b36e744e
Juan Diego Bernuy Rey <JD>
2023-10-10 20:18:31 -0500

    jd10

--- src/app/components/FormCuenta/FormCuenta.jsx
+++ src/app/components/FormCuenta/FormCuenta.jsx
@@ -12,7 +12,7 @@ function FormCuenta() {
                 </li>
             </ul>
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:ring-0">
-                Button
+                Guardar
             </button>
         </div>
         </>
