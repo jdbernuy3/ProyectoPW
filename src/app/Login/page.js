0f6c3b9ac4a6772810971f997c52dbf9b46f81a2
Fdonayre1~ <franciscodonsal@gmail.com>
2023-10-13 14:32:13 -0500

    cambiops1

---
+++ src/app/Login/page.js
@@ -0,0 +1,43 @@
+'use client';
+
+import StyleL from 'C:\Users\PC\Desktop\TRABAJO\ProyectoPW\src\app\Login\styleL.css'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+
+const Login = () => {
+    Inicio("/Login");
+    const [usuario, setUsuario] = useState('')
+    const [password, setPassword] = useState('')
+
+    const router = useRouter()
+
+    const handleClick = () => {
+
+        const data = JSON.parse(window.localStorage.getItem('personas')) //cambio
+        let foundusuario = null
+        if (data) {
+            foundusuario = data.find(
+                (item) => item.correo === usuario && item.contrasena === password //cambio
+            )
+            
+            //cambio
+            if(foundusuario){
+                //Conseguir el dato de nombres
+                window.localStorage.setItem('nombreUsuario', foundusuario.nombres);
+                //Conseguir el dato de rol
+                window.localStorage.setItem('rol', foundusuario.rol);
+                window.localStorage.setItem("recargar", "true");
+                //window.location.replace("/UsuarioPrincipal")
+                router.push('/UsuarioPrincipal')
+            }else{
+                alert("Correo o contraseña incorrectos");
+            }
+            //cambio
+                
+        }else{
+            alert("Actualmente no existen cuentas");
+        }
+}
+
+} 
