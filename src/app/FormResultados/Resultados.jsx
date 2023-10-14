206646e31bb7f1d18a254d030194775497792263
MariaPaz <20200579@aloe.ulima.edu.pe>
2023-10-14 02:19:28 -0500

    posible busqueda

--- src/app/FormResultados/Resultados.jsx
+++ src/app/FormResultados/Resultados.jsx
@@ -1,9 +1,6 @@
 
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Image from "next/image";
-import TextField from "@mui/material/TextField"
-import { InputAdornment } from "@mui/material/InputAdornment";
 
 
 
@@ -21,7 +18,8 @@ const commonStyles = {
 			<Box sx={{ ...commonStyles, borderColor: 'grey.500','borderRadius':'5px'}} >
       <Typography >Psychology of Computer Programming</Typography>
         <img inline-block className='h-40 w-40'
-         src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/49420/man-student-emoji-clipart-md.png"
+         src="https://images.cdn2.buscalibre.com/fit-in/180x180/1f/a6/1fa666e0f93fb0bc63c4c214188f3a46.jpg"
+
          ></img>
       <Typography >ISBN: 00000</Typography>
       <Typography >Author: 09322633420</Typography>
@@ -31,7 +29,7 @@ const commonStyles = {
 
 			<Box sx={{ ...commonStyles, borderColor: 'grey.500','borderRadius':'5px' }}>
       <Typography >Psychology of Computer Programming</Typography>
-        <img inline-block className='h-40 w-40' src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/49420/man-student-emoji-clipart-md.png"></img>
+        <img inline-block className='h-40 w-40' src="https://images.cdn3.buscalibre.com/fit-in/180x180/f8/78/f878362b2a6c71f5e7125eafff09a82c.jpg"></img>
       <Typography >ISBN: 00000</Typography>
       <Typography >Author: 09322633420</Typography>
       <Typography >Editor: Adison</Typography>
@@ -40,8 +38,8 @@ const commonStyles = {
         </Box>
 			<Box sx={{ ...commonStyles, borderColor: 'grey.500','borderRadius':'5px' }}>
       <Typography >Psychology of Computer Programming</Typography>
-        <img inline-block className='h-40 w-40' src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/49420/man-student-emoji-clipart-md.png"></img>
-      <Typography >ISBN: 00000</Typography>
+        <img inline-block className='h-40 w-40' src="https://images.cdn1.buscalibre.com/fit-in/180x180/b2/4f/b24f5a1c7adddcc154ad324483235c72.jpg"></img>
+        <Typography >ISBN: 00000</Typography>
       <Typography >Author: 09322633420</Typography>
       <Typography >Editor: Adison</Typography>
       <button class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-2 rounded-full focus:ring-0" >Reservar</button>
--- src/app/busqueda/page.jsx
+++ src/app/busqueda/page.jsx
@@ -7,6 +7,8 @@ import FormPreferencias from "@/app/components/FormPreferencias/FormPreferencias
 import FormInsertarRecurso from "../components/FormInsertarRecurso/FormInsertarRecurso"
 import InsertarRecurso from "../components/FormInsertarRecursoBusqueda/FormInsertarRecursoBusqueda";
 import FormInsertarRecursoBusqueda from "../components/FormInsertarRecursoBusqueda/FormInsertarRecursoBusqueda";
+import React, { useState } from 'react';
+
 
 
     const busquedaAdm = () => {
@@ -35,3 +37,47 @@ import FormInsertarRecursoBusqueda from "../components/FormInsertarRecursoBusque
     
 
 export default busquedaAdm
+
+/*
+const busquedaAdm = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [items, setItems] = useState([]); 
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // Realiza la búsqueda en los datos almacenados en `items`
+    const results = items.filter((item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    console.log('Resultados de la búsqueda:', results);
+  };
+
+ 
+
+  return (
+    <>
+      <MyAppBar />
+      <div className="bg-white h-100v w-100v pl-52 pr-8">
+        <h1 className="pt-10 text-4xl">Busqueda</h1>
+        <hr className="my-8 h-0.5 border-t-0 bg-[#CAC4D0] opacity-100" />
+        <div className="h-100v w-100v bg-[#F3EDF7] flex p-10 space-x-2">
+          <div>
+            <input
+              type="text"
+              placeholder="Buscar elementos"
+              value={searchTerm}
+              onChange={handleInputChange}
+            />
+            <button onClick={handleSearch}>Buscar</button>
+          </div>
+          { <FormInsertarRecursoBusqueda onAddItem={handleAddItem} /> }
+        </div>
+      </div>
+    </>
+  );
+};
+
+*/
