ef0c1a757b6e1af4d03974ba452559681d2bf584
Juan Diego Bernuy Rey <JD>
2023-11-29 21:52:19 -0500

    jd1

---
+++ src/api/usuario.js
@@ -0,0 +1,15 @@
+import base from './base.js';
+
+const endpoint = '/usuario';
+
+const findAll = async () => await base.get(endpoint);
+
+const findOne = async (userId) => await base.get(endpoint,userId);
+
+const update = async (payload) => await base.put(endpoint, payload);
+
+const create = async (payload) => await base.post(endpoint, payload);
+
+const api = { findAll, findOne, update, create };
+
+export default api;
