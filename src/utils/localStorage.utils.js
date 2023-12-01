61089506ecb6f0785cfe6ed0d73e8d5051c38ab9
Donayre <20180616@aloe.ulima.edu.pe>
2023-12-01 17:44:47 -0500

    update: LS

--- src/utils/localStorage.utils.js
+++ src/utils/localStorage.utils.js
@@ -2,16 +2,16 @@
 // Almacenar información de usuario en localStorage
 export const guardarInformacionUsuario = (id,correo, contrasena, nombre, apellidos, fotoUrl, idTipoUsuario, idTipoDoc, nroDoc, idioma, prefijo, color) => {
   const usuario = { id,correo, contrasena, nombre, apellidos, fotoUrl, idTipoUsuario, idTipoDoc, nroDoc, idioma, prefijo, color };
-  localStorage.setItem('users', JSON.stringify(usuario));
+  localStorage.setItem('user', JSON.stringify(usuario));
 };
 
 // Obtener información de usuario desde localStorage
 export const obtenerInformacionUsuario = () => {
-  const usuarioAlmacenado = localStorage.getItem('users');
+  const usuarioAlmacenado = localStorage.getItem('user');
   return usuarioAlmacenado ? JSON.parse(usuarioAlmacenado) : null;
 };
 
 // Eliminar información de usuario de localStorage
 export const eliminarInformacionUsuario = () => {
-  localStorage.removeItem('users');
+  localStorage.removeItem('user');
 };
