d01615a6815c1e53b1995d8bf75ede44d87f1f0c
Juan Diego Bernuy Rey <JD>
2023-11-30 20:51:20 -0500

    jd14

--- src/api/tipoDoc.js
+++ src/api/tipoDoc.js
@@ -10,7 +10,7 @@ const update = async (payload) => await base.put(endpoint, payload);
 
 const create = async (payload) => await base.post(endpoint, payload);
 
-const remove = async (payload) => await base.remove(endpoint, payload);
+const remove = async (id) => await base.remove(endpoint + '/' + id);
 
 const api = { findAll, findOne, update, create, remove};
 
